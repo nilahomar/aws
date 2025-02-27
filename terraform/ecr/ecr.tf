3f7b87c5e514385f5aebf2990c58dab589e11248
Nila Homar <47689055+nilahomar@users.noreply.github.com>
2025-02-27 21:15:19 +0100

    ecr repos (#4)

---
+++ terraform/ecr/ecr.tf
@@ -0,0 +1,10 @@
+locals {
+  repositories = ["client", "server", "worker"]
+}
+
+resource "aws_ecr_repository" "this" {
+  for_each = toset(local.repositories)
+
+  name                 = each.value
+  image_tag_mutability = "MUTABLE"
+}
